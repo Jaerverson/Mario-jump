540ca8ad54535e6b933c9d1cbefd6e630c615637
Jaérverson <83619762+Jaerverson@users.noreply.github.com>
2023-03-01 00:39:18 -0300

    mario hit-box pipe

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+
+
+    }, 500);
+}
+const loop = setInterval(() =>{
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+        if(pipePosition <= 80 && pipePosition > 0 && marioPosition <= 90){
+
+            pipe.style.animation ='none';
+            pipe.style.left = '${pipeposition}px';
+
+        }
+
+
+}, 10);
+
+document.addEventListener('keydown', jump);
